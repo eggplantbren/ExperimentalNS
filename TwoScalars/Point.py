e06ef91367996e7f39a62d275eb73759b13dca35
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-01-10 11:51:00 +1300

    A point in parameter space

---
+++ TwoScalars/Point.py
@@ -0,0 +1,30 @@
+import numpy as np
+import numpy.random as rng
+
+class Point:
+	"""
+	Represents a point in parameter space.
+	"""
+	def __init__(self, num_params=100):
+		self.num_params = num_params
+		self.x = np.empty(self.num_params)
+
+	def perturb(self):
+		"""
+		Does a metropolis-hastings proposal that respects the prior (DNest
+		style).
+		"""
+		which = rng.randint(self.num_params)
+		self.x[which] += 10.**(1.5 - 6.*rng.rand())*rng.randn()
+		self.x[which] = np.mod(self.x[which], 1.)
+		return 0.
+
+	@property
+	def scalars(self):
+		"""
+		Evaluate all the relevant scalars.
+		"""
+		return [np.sum(np.abs(self.x - 0.25)),\
+			np.sum(np.abs(self.x - 0.75))]
+
+
