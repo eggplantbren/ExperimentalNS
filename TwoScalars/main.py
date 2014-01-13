f646a6d2411f6cd707a8b8efbe2d02b7295a6267
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-01-13 21:37:10 +1300

    Do ten runs, save them as pickles

--- TwoScalars/main.py
+++ TwoScalars/main.py
@@ -1,25 +1,30 @@
 from Walker import *
 import matplotlib.pyplot as plt
 
-walker = Walker()
-walker.initialise()
-print(walker.direction)
+def do_run(filename):
+	walker = Walker()
+	walker.initialise()
+	print(walker.direction)
 
-plt.ion()
-plt.hold(False)
-for i in xrange(0, 10000):
-	success = walker.advance()
+	plt.ion()
+	plt.hold(False)
+	for i in xrange(0, 100):
+		success = walker.advance()
 
-	plt.hist(walker.point.x, 30)
-	plt.title('After {k} iterations, direction = [{a:.3f}, {b:.3f}]'\
-			.format(k=i+1, a=walker.direction[0],\
-					b=walker.direction[1]))
-	plt.xlim([0., 1.])
-	plt.draw()
+		plt.hist(walker.point.x, 30)
+		plt.title('After {k} iterations, direction = [{a:.3f}, {b:.3f}]'\
+				.format(k=i+1, a=walker.direction[0],\
+						b=walker.direction[1]))
+		plt.xlim([0., 1.])
+		plt.draw()
 
-	if success is False:
-		break
+		if success is False:
+			break
 
-plt.ioff()
-plt.show()
+	import cPickle as pickle
+	pickle.dump(walker, open(filename, 'wb'))
+
+if __name__ == '__main__':
+	for i in xrange(0, 10):
+		do_run('walker' + str(i+1) + '.dat')
 
