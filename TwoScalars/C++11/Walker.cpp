60ba3ce5c60b230f3f198e11e8523eadeec37ff7
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-01-15 16:02:31 +1300

    Choose a direction

--- TwoScalars/C++11/Walker.cpp
+++ TwoScalars/C++11/Walker.cpp
@@ -1,4 +1,6 @@
 #include "Walker.h"
+#include "RNG.h"
+#include <iostream>
 
 template<class Type>
 Walker<Type>::Walker()
@@ -10,7 +12,21 @@ Walker<Type>::Walker()
 template<class Type>
 void Walker<Type>::initialise()
 {
+	// Choose direction from a dirichlet(1,...,1) distribution
+	double total = 0.;
+	for(double& d : direction)
+	{
+		d = -log(RNG::rand());
+		total += d;
+	}
+	for(double& d : direction)
+		d /= total;
+
 	point.from_prior();
 	point.from_prior_tiebreakers();
+
+	std::cout<<"# Initialised a walker with direction: ";
+	for(double d : direction)
+		std::cout<<d<<"   ";
 }
 
