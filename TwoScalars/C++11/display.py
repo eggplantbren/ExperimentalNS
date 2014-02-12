e0e5d28b9723192943fd43af37410432cb5180a3
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-02-12 13:08:46 +1300

    Do ten walkers

--- TwoScalars/C++11/display.py
+++ TwoScalars/C++11/display.py
@@ -1,15 +1,16 @@
 # Display file for Potts
 from pylab import *
 
-output = atleast_2d(loadtxt('Output/output0.txt'))
+for k in xrange(0, 1000):
+	output = atleast_2d(loadtxt('Output/output' + str(k) + '.txt'))
 
-ion()
-hold(False)
-for i in xrange(0, output.shape[0]):
-	x = output[i, 0:-2].reshape((32, 32))
-	imshow(x, interpolation='nearest')
-	title(i+1)
-	draw()
+	ion()
+	hold(False)
+	for i in xrange(0, output.shape[0]):
+		x = output[i, 0:-2].reshape((32, 32))
+		imshow(x, interpolation='nearest')
+		title('Walker ' + str(k) + ', iteration ' + str(i+1))
+		draw()
 
 ioff()
 show()
--- TwoScalars/C++11/main.cpp
+++ TwoScalars/C++11/main.cpp
@@ -10,7 +10,9 @@ using namespace std;
 int main()
 {
 	RNG::seed(0);
-	launch_walker<Potts>(0, 2000, 10000, 10);
+
+	for(int i=0; i<10; i++)
+		launch_walker<Potts>(i, 2000, 10000, 10);
 	return 0;
 }
 
