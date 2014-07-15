52a9df1705fcc70ae0e17db8a729be7715e58e20
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-07-15 10:29:07 -0500

    Implemented 'Example' in DNest

--- TwoScalars/DNest/MyModel.cpp
+++ TwoScalars/DNest/MyModel.cpp
@@ -7,28 +7,61 @@ using namespace std;
 using namespace DNest3;
 
 MyModel::MyModel()
+:params(1000)
+,scalars(2)
 {
 
 }
 
 void MyModel::fromPrior()
 {
+	for(size_t i=0; i<params.size(); i++)
+		params[i] = randomU();
 
+	compute_scalars();
 }
 
 double MyModel::perturb()
 {
+	int reps = (int)(pow(10., -3.*randomU())*params.size());
+	if(reps == 0)
+		reps = 1;
+	if(reps == (int)params.size())
+		reps = params.size();
+
+	int which;
+	for(int i=0; i<reps; i++)
+	{
+		which = randInt(params.size());
+		params[which] += randh();
+		params[which] = mod(params[which], 1.);
+	}
+
+	compute_scalars();
+
 	return 0.;
+}
+
+void MyModel::compute_scalars()
+{
+	scalars[0] = 0.;
+	for(size_t i=0; i<params.size(); i++)
+		scalars[0] += -pow(params[i] - 0.5, 2);
+
+	scalars[1] = 0.;
+	for(size_t i=0; i<params.size(); i++)
+		scalars[1] += -pow(sin(2.*M_PI*params[i]/0.5), 2);
 }
 
 double MyModel::logLikelihood() const
 {
-	return 0.;
+	return scalars[0];
 }
 
 void MyModel::print(std::ostream& out) const
 {
-
+	for(size_t i=0; i<params.size(); i++)
+		out<<params[i]<<' ';
 }
 
 string MyModel::description() const
