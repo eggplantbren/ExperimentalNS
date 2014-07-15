dff7947f5d23e75d0d75c4285a21f16766de5b4d
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-07-15 11:43:07 -0500

    Working on showeresults

--- TwoScalars/DNest/showresults.py
+++ TwoScalars/DNest/showresults.py
@@ -1,26 +1,16 @@
-# Copyright (c) 2009, 2010, 2011, 2012 Brendon J. Brewer.
-#
-# This file is part of DNest3.
-#
-# DNest3 is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# DNest3 is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with DNest3. If not, see <http://www.gnu.org/licenses/>.
-
-# Import postprocess from two directories up
-# http://stackoverflow.com/a/9806045
-import os
-parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-parentdir = os.path.dirname(parentdir)
-os.sys.path.insert(0, parentdir) 
+from pylab import *
 import postprocess
-postprocess.postprocess()
+
+# Load the levels from the DNest run
+levels = loadtxt('levels.txt')
+sample_info = loadtxt('sample_info.txt')
+
+# Get logx of samples from the DNest run
+result = postprocess.postprocess()
+
+# Relationship between logl and logx based on the DNest run
+logx = hstack([levels[:,0], result[2].flatten()])
+logl = vstack([levels[:,1:3], sample_info[:,1:3]])
+
+print(logl)
 
