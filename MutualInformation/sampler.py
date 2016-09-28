265988116e2f492f488bb2705aa71d36ae95c0a0
Brendon Brewer <brendon.brewer@gmail.com>
2016-09-28 21:56:14 +1300

    Bit of work

--- MutualInformation/sampler.py
+++ MutualInformation/sampler.py
@@ -1,5 +1,6 @@
 """
 Define the Sampler class.
+For now, this is just standard NS.
 """
 
 __all__ = ["Sampler"]
@@ -11,7 +12,16 @@ class Sampler:
     """
     An object of this class is a sampler.
     """
-    def __init__(self):
-        pass
+    def __init__(self, num_particles=10):
+        """
+        Constructor: specify number of particles.
+        """
+        self.ready = False
 
+    def initialise(self):
+        """
+        Generate particles from the prior.
+        """
+        self.particles = [from_prior() for i in range(0, self.num_particles)]
+        self.ready = True
 
