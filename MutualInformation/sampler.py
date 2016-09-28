54d05d2047d06b8c99b321eb676b2621ca2e523e
Brendon Brewer <brendon.brewer@gmail.com>
2016-09-28 21:53:15 +1300

    Sampler file

---
+++ MutualInformation/sampler.py
@@ -0,0 +1,17 @@
+"""
+Define the Sampler class.
+"""
+
+__all__ = ["Sampler"]
+
+# Import the model
+from model import from_prior, log_prior, log_likelihood
+
+class Sampler:
+    """
+    An object of this class is a sampler.
+    """
+    def __init__(self):
+        pass
+
+
